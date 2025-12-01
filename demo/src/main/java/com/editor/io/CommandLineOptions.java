0164be28b3a0c8e5c3a703d336f0f5ea2dd3710a
Anas Khaled <anaskhaled23.9.4@gmail.com>
2025-12-01 11:32:14 +0200

    CLI ARGS Parsing Logic (Not Finished)

--- demo/src/main/java/com/editor/io/CommandLineOptions.java
+++ demo/src/main/java/com/editor/io/CommandLineOptions.java
@@ -12,13 +12,39 @@ public class CommandLineOptions {
     private String word;
     private String topic;
 
+    private CommandLineOptions() {}
+
     public static CommandLineOptions parse(String[] args)
     {
+        CommandLineOptions options = new CommandLineOptions();
+        options.command = args[0];
 
+        for(int i = 1; i < args.length; i++){
+            if(args[i].equals("-i") && i+1 < args.length){
+                options.inputPath = args[i+1];
+                i++;
+            }
+            else if(args[i].equals("-o") && i+1 < args.length){
+                options.outputPath = args[i+1];
+                i++;
+            }
+
+            // Need to add the other arguments same way.
+        }
+
+
+        return options;
     }
-    String getCommand()
+
+    public String getCommand()
     {
         return command;
+    }
+    public String getInputPath(){
+        return inputPath;
+    }
+    public String getOutputPath(){
+        return outputPath;
     }
     // Getters for other fields...
     //handle FIXing and any needed methods
