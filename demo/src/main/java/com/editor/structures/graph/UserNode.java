2e9f97c3e070b2f3f1025cdef2a50996efab8ada
kareem <you@example.com>
2025-12-01 23:28:40 +0200

    add setters and getters for usernode

--- demo/src/main/java/com/editor/structures/graph/UserNode.java
+++ demo/src/main/java/com/editor/structures/graph/UserNode.java
@@ -10,7 +10,32 @@ public class UserNode {
     private List<Post> posts;
     private List<Integer> followerIds; // raw IDs from XML
 
-
+    public UserNode(){
+        posts = new ArrayList<>();
+        followerIds = new ArrayList<>();}
+    //setters and getters
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public List<Post> getPosts() {
+        return posts;
+    }
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
+    public List<Integer> getFollowerIds() {
+        return followerIds;
+    }
+    public void setFollowerIds(List<Integer> followerIds) {this.followerIds = followerIds;}
     public void addPost(Post post) {
         this.posts.add(post);
     }
