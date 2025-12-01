77dc96f4a3b728cea96d46c2461c35d0bb2249bb
zaki mahmoud <zakirokba@gmail.com>
2025-12-02 00:36:17 +0200

    add XmlToJson function

--- demo/src/main/java/com/editor/xml/converter/XmlToJson.java
+++ demo/src/main/java/com/editor/xml/converter/XmlToJson.java
@@ -1,16 +1,29 @@
 package com.editor.xml.converter;
+import com.editor.structures.graph.UserNode;
 import com.editor.structures.xml.XmlDocument;
 import com.editor.structures.xml.XmlNode;
-public class XmlToJson {
-    public String toJson(XmlDocument doc)
-    {
-        // Dummy implementation for demonstration purposes
-        return "{\"dummy_key\": \"dummy_value\"}";
-    }
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
-    private String nodeToJson(XmlNode node)
-    {
-        // Dummy implementation for demonstration purposes
-        return "{\"node_key\": \"node_value\"}";
+public class XmlToJson {
+    public String toJson(List users) {
+        try{
+            ObjectMapper mapper = new ObjectMapper();
+            ObjectWriter writer = mapper.writerWithDefaultPrettyPrinter();
+            return writer.writeValueAsString(users);
+        }
+        catch (Exception e) {e.printStackTrace();}
+        return null;
     }
+    private String nodeToJson(UserNode user) {
+        try{
+            ObjectMapper mapper = new ObjectMapper();
+            ObjectWriter writer = mapper.writerWithDefaultPrettyPrinter();
+            return writer.writeValueAsString(user);
+        }
+        catch (Exception e) {e.printStackTrace();}
+        return null;}
 }
