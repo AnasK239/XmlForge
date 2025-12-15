e88e718a04ae6f76f3f4c781bf8b734fd55e7a2d
Anas Khaled <anaskhaled23.9.4@gmail.com>
2025-12-15 04:38:35 +0200

    Implementation of core graph structures

--- demo/src/main/java/com/editor/structures/graph/Graph.java
+++ demo/src/main/java/com/editor/structures/graph/Graph.java
@@ -1,28 +1,66 @@
 package com.editor.structures.graph;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Graph {
-    //Our own graph DS: adjacency list of followers.
 
-    private int[] userIds; // index → userId
+    // Not used (dont know if we will need it yet)
+    private int[] userIds;
 
     private int size; // number of users
+    private List<Integer>[] adj; // adjacency list: adj[i] = List of (followers or followings ?)
 
-    private List<Integer>[] adj; // adjacency list: adj[i] = followers or followings
+    public Graph(int maxNodes) {
+        this.size = maxNodes+1;
+        this.adj = new ArrayList[size];
+        for(int i = 0; i < size; i++){
+            adj[i] = new ArrayList<>();
+        }
+    }
 
-    //or build from list
-    public Graph(int maxNodes) {}
+    public void addNode(int userId){
+        if(userId >= adj.length){
+            resize(userId*2);
+        }
+    }
 
-    public void addNode(int userId){}
+    public void addEdge(int fromUserId , int toUserId ){
+        // Make sure both are in the bounds of graph size
+        addNode(fromUserId);
+        addNode(toUserId);
 
-    public void addEdge(int fromUserId, int toUserId){}
+        if (!adj[fromUserId].contains(toUserId)) {
+            adj[fromUserId].add(toUserId);
+        }
+    }
+
+    // If user id bigger than the current capacity , the adj list is doubled
+    private void resize(int newCapacity) {
+        List<Integer>[] newAdj = new ArrayList[newCapacity];
+        for (int i = 0; i < adj.length; i++) {
+            newAdj[i] = adj[i];
+        }
+        for (int i = adj.length; i < newCapacity; i++) {
+            newAdj[i] = new ArrayList<>();
+        }
+        this.adj = newAdj;
+    }
+
 
     public List<Integer> getNeighbors(int userId){
-        return null;
+        if(userId < adj.length){
+            return adj[userId];
+        }
+        return new ArrayList<>();
     }
 
     public int getDegree(int userId) {
-        return 0;
-    }// for activity
+        return getNeighbors(userId).size();
+    }
+
+    // probably will not need this because UserDirectory has all users
+    public int getSize() {
+        return adj.length;
+    }
 }
--- demo/src/main/java/com/editor/structures/graph/SocialNetwork.java
+++ demo/src/main/java/com/editor/structures/graph/SocialNetwork.java
@@ -1,9 +1,33 @@
 package com.editor.structures.graph;
 
+// SocialNetwork is the entire thing the graph structure + the database of users
+// it's an interface to interact with both at the same time ( add to the graph and database )
 public class SocialNetwork {
-    // Represents the social network graph.
-    UserDirectory userDirectory;
-    Graph graph;
 
-    // add what is needed
+    private UserDirectory userDirectory;
+    private Graph graph;
+
+    private int initialCap = 100;
+
+    public SocialNetwork() {
+        userDirectory = new UserDirectory();
+        graph = new Graph(initialCap);
+    }
+
+    public void addUser(UserNode user) {
+        userDirectory.addUser(user);
+        graph.addNode(user.getId());
+    }
+
+    public void addConnection(int followerId, int followeeId) {
+        graph.addEdge(followerId, followeeId);
+    }
+
+    public UserDirectory getUserDirectory() {
+        return userDirectory;
+    }
+
+    public Graph getGraph() {
+        return graph;
+    }
 }
